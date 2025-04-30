35ea083f899c52ba27fa67f53ddf893bc826980e
Swapna Iyer <swapna.iyer@gmail.com>
2025-04-29 19:05:42 -0700

    vercel adaptor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+ let name = 'world';
+</script>
+
+<main>
+  <h1> Hello {name} </h1>
+</main>
+

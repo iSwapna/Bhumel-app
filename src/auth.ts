bca70096b6e6355fc5c1665f02bf118870494b79
Swapna Iyer <swapna.iyer@gmail.com>
2025-05-01 00:45:34 -0700

    Format

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,18 @@
 // @ts-nocheck
-import { SvelteKitAuth } from "@auth/sveltekit";
-import GitHub from "@auth/sveltekit/providers/github"
-import { AUTH_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private";
- 
+import { SvelteKitAuth } from '@auth/sveltekit';
+import GitHub from '@auth/sveltekit/providers/github';
+import { AUTH_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
+
 export const { handle, signIn, signOut } = SvelteKitAuth(async (event) => {
-  const authOptions = {
-    providers: [
-      GitHub({
-        clientId: GITHUB_CLIENT_ID,
-        clientSecret: GITHUB_CLIENT_SECRET
-      })
-    ],
-    secret: AUTH_SECRET,
-    trustHost: true
-  }
-  return authOptions
-})
+	const authOptions = {
+		providers: [
+			GitHub({
+				clientId: GITHUB_CLIENT_ID,
+				clientSecret: GITHUB_CLIENT_SECRET
+			})
+		],
+		secret: AUTH_SECRET,
+		trustHost: true
+	};
+	return authOptions;
+});
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,7 @@
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
-    return {
-        session: await event.locals.auth()
-    };
+	return {
+		session: await event.locals.auth()
+	};
 };

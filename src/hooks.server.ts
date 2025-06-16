a971a947db138a8472947fbf3dba74ce7a3d427c
Swapna Iyer <swapna.iyer@gmail.com>
2025-06-16 13:59:59 -0700

    csp stuff

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,8 +9,8 @@ const handleCSP: Handle = async ({ event, resolve }) => {
 
 	// Add CSP headers based on environment
 	const csp = dev
-		? "default-src 'self' https:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https:; style-src-elem 'self' 'unsafe-inline' https:; img-src 'self' data: https:; connect-src 'self' https:; frame-src 'self' https:;"
-		: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https:; style-src-elem 'self' 'unsafe-inline' https:; img-src 'self' data: https://authjs.dev https://i.pravatar.cc https://*.pravatar.cc https://pravatar.cc; connect-src 'self' https://api.yog.dev https://api.yog.dev/ https://api.yog.dev/* https://soroban-testnet.stellar.org https://soroban-testnet.stellar.org/ https://soroban-testnet.stellar.org/*; frame-src 'self' https://authjs.dev;";
+		? "default-src 'self' https:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https:; style-src-elem 'self' 'unsafe-inline' https:; img-src 'self' data: https:; connect-src 'self' https:; frame-src 'self' https:; font-src 'self' https://cdn.jsdelivr.net;"
+		: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https:; style-src-elem 'self' 'unsafe-inline' https:; img-src 'self' data: https://authjs.dev https://i.pravatar.cc https://*.pravatar.cc https://pravatar.cc; connect-src 'self' https://api.yog.dev https://api.yog.dev/ https://api.yog.dev/* https://soroban-testnet.stellar.org https://soroban-testnet.stellar.org/ https://soroban-testnet.stellar.org/*; frame-src 'self' https://authjs.dev; font-src 'self' https://cdn.jsdelivr.net;";
 
 	response.headers.set('Content-Security-Policy', csp);
 
